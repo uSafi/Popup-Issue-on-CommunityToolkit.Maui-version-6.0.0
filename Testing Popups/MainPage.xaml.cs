285159f02e9726576391ee80f80379a7ce3ac424
Usama.S.Y <43465134+uSafi@users.noreply.github.com>
2023-09-30 19:58:10 +0300

    Add project files.

---
+++ Testing Popups/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace Testing_Popups
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnDisplayPopupClicked(object sender, EventArgs e)
+        {
+            await DisplayPopup();
+        }
+        public async static Task DisplayPopup()
+        {
+            var popup = new SimplePopup();            
+            await App.Current.MainPage.ShowPopupAsync(popup);
+        }
+    }
+}
